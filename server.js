0abe7f512c07bb230eb760fefd03e1d95fe520c3
siddhi22rachit <siddhibswant2212@gmail.com>
2024-12-11 15:20:39 +0530

    server add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import schoolRoutes from './routes/schoolRoutes.js';
+import schoolRoutes from './routes/schoolRoutes.js'
 
 dotenv.config();
 
